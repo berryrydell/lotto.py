4119a972822d19e601a7c969b9ceafdeb6b973f7
berryrydell <greedypiglet@o2.pl>
2017-03-22 23:37:29 +0100

    Removed commented lines and introduced sorting

--- lotto.py
+++ lotto.py
@@ -4,11 +4,12 @@ import os
 
 print "wygeneruje liste 8 losowych liczb z zakresu 1-49"
 
-#zestaw=[]
 nums=(range(1,50))
 lista=[]
 for x in range(8):
     y=random.choice(nums)
     lista.append(y)
     nums.remove(y)
-print lista
+final=sorted(lista)
+print final
+
